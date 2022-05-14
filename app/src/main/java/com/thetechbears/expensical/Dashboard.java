57c6aca596603ed473f0717f6932bcf09941771f
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2022-05-14 19:24:34 -0400

    Chart Library Add

--- app/src/main/java/com/thetechbears/expensical/Dashboard.java
+++ app/src/main/java/com/thetechbears/expensical/Dashboard.java
@@ -7,7 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -17,6 +24,9 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         Button logout = findViewById(R.id.logout);
+        PieChart pieChart = findViewById(R.id.pieChart);
+
+//        Logout Button
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +37,22 @@ public class Dashboard extends AppCompatActivity {
                 finish();
             }
         });
+
+//        Charts
+        ArrayList<PieEntry> pieEntries = new ArrayList<>();
+
+        for (int i = 0; i<10; i++){
+            float value = (float) (i*10.0);
+            PieEntry pieEntry = new PieEntry(i, value);
+            pieEntries.add(pieEntry);
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "Employees");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieChart.setData(new PieData(pieDataSet));
+        pieChart.animateXY(5000, 5000);
+        pieChart.getDescription().setEnabled(false);
+
+
     }
 }
